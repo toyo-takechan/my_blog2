5a402b6bdecb8a461290f220804e7c4b5b3fa91d
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-02-14 01:36:13 +0900

    new.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,14 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
