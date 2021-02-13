d6dedbf5cfad0a908d86f44a88ee54986c024756
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-02-13 17:36:22 +0900

    flame2

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   root to: 'users#index'
   resources :users
+  resources :posts
 end
