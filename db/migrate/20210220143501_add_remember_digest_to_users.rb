2f41ff553596b811c2b4be945da30e8dbd2013cf
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-02-20 23:37:54 +0900

    migrationteisei

--- db/migrate/20210220143501_add_remember_digest_to_users.rb
+++ db/migrate/20210220143501_add_remember_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddRememberDigestToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :remember_digest, :stiring
+    add_column :users, :remember_digest, :string
   end
 end
